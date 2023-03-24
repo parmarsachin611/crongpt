9160cbbee1a69b72530ad17b2244160df8ea3331
sachinparmar611 <parmarsachin707@gmail.com>
2023-03-24 17:02:58 +0530

    .

--- server/server.js
+++ server/server.js
@@ -13,7 +13,13 @@ const openai = new OpenAIApi(configuration);
 
 const app = express();
 
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
